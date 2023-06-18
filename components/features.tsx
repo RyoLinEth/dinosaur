e2b2c17494ef969dc35903c2ea63ec400b85c15b
RyoLinEth <ryolindev@gmail.com>
2023-06-18 08:50:13 +0800

    +RoadMap

--- components/features.tsx
+++ components/features.tsx
@@ -80,39 +80,40 @@ export default function Features() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+          <div className="py-12 md:py-20">
+            {/* Section header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+              <h2 className="h2 mb-4">About Us.</h2>
+              <p className="text-xl text-gray-400">
+                Every boy has a dinosaur in his heart. <br />
+                Even though we grow up, the dinosaur in our heart never dies. <br />
+                Remind you with the cute dinosaur, to memorize self in youth<br />
+              </p>
+            </div>
 
-          {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">About Us.</h2>
-            <p className="text-xl text-gray-400">
-              Every boy has a dinosaur in his heart. <br />
-              Even though we grow up, the dinosaur in our heart never dies. <br />
-              Remind you with the cute dinosaur, to memorize self in youth<br />
-            </p>
+            {/* Items */}
+            <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
+              {
+                items.map((item, index) => {
+                  return (
+                    <div
+                      key={item.title}
+                      className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+                      {item.svgPath}
+                      <h4 className="h4 mb-2">
+                        {item.title}
+                      </h4>
+                      <p className="text-lg text-gray-400 text-center">
+                        {item.description}
+                      </p>
+                    </div>
+                  )
+                })
+              }
+            </div>
+
           </div>
-
-          {/* Items */}
-          <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
-            {
-              items.map((item, index) => {
-                return (
-                  <div
-                    key={item.title}
-                    className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
-                    {item.svgPath}
-                    <h4 className="h4 mb-2">
-                      {item.title}
-                    </h4>
-                    <p className="text-lg text-gray-400 text-center">
-                      {item.description}
-                    </p>
-                  </div>
-                )
-              })
-            }
-          </div>
-
         </div>
       </div>
     </section>
--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -28,12 +28,11 @@ export default function Zigzag() {
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20 border-t border-gray-800">
-
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
+            {/* <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
               The stuff you should care
-            </div>
+            </div> */}
             <h1 className="h2 mb-4">
               Tokenomics
             </h1>
